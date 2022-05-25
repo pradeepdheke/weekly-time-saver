8da8d9f1a0d310be15d8f1cce2e1e6dd61501df7
pradeep dheke <pradeepbarca029@gmail.com>
2022-05-25 15:56:25 +1000

    added delete function and switch function in both list

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let entryList = []
+let badList = []
+
+// get data on form submit
+
 const handleOnSubmit = e => {
     
     const formDt = new FormData(e)
@@ -10,6 +14,7 @@ const handleOnSubmit = e => {
    display(entryList)
 }
 
+// display the data in the list 
 const display = (taskArg) => {
     let str = ""
 
@@ -24,7 +29,7 @@ taskArg.map((item, i) => {
 <button onclick = (handleOnDeleteEntryList(${i})) class="btn btn-danger">
     <i class="fa-solid fa-trash-can"></i>
 </button>
-<button class="btn btn-success">
+<button onclick = (switchToBadList(${i})) class="btn btn-success">
     <i class="fa-solid fa-arrow-right"></i>
 </button>
 </td>
@@ -34,6 +39,10 @@ document.getElementById('entryList').innerHTML = str
 
 }
 
+
+
+
+// delete data from the entry list
 const handleOnDeleteEntryList = i => {
 
     if(!confirm("Are you sure you to delete this entry")) return;
@@ -41,4 +50,51 @@ const handleOnDeleteEntryList = i => {
 
     entryList = filteredArg
     display(entryList)
+}
+
+// switch data to the bad list
+const switchToBadList = (i) => {
+    const itemToBeSwitched = entryList.splice(i, 1)
+    badList.push(itemToBeSwitched[0])
+    display(entryList)
+    badListDisplay(badList)
+}
+
+// display bad list on the dom
+const badListDisplay = (arg) => {
+    let str = ""
+
+    arg.map((item, i)=> {
+        str += `<tr>
+        <td> ${item.task}</td>
+        <td>${item.hr}hrs</td>
+        <td class="text-end">
+        <button onclick= (switchToEntryList(${i})) class="btn btn-warning">
+        <i class="fa-solid fa-arrow-left"></i>
+        </button>
+        <button onclick= (handleOnDeleteBadList(${i})) class="btn btn-danger">
+                <i class="fa-solid fa-trash-can"></i>
+            </button>
+        </td>
+        </tr>`
+    })
+    document.getElementById('badList').innerHTML = str
+}
+
+// delete data from the entry list
+const handleOnDeleteBadList = i => {
+
+    if(!confirm("Are you sure you to delete this entry")) return;
+    const filteredarg = badList.filter((item, index) => index !== i)
+
+    badList = filteredarg
+    badListDisplay(badList)
+}
+
+// switch data to the entry list
+const switchToEntryList = (i) => {
+    const itemToBeSwitched = badList.splice(i, 1)
+    entryList.push(itemToBeSwitched[0])
+    display(entryList)
+    badListDisplay(badList)
 }
